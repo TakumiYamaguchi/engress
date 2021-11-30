af9f01cef0513268ab5d2411f5502dade49669fb
TakumiYamaguchi <info@ivorytowerlab.jp>
2021-11-30 16:18:09 +0900

    first commit

---
+++ template/document_request.php
@@ -0,0 +1,12 @@
+<div id="document_request">
+    <div class="document_request__inner">
+        <h2>まずは無料で資料請求から</h2>
+        <a class="btn btn_contents" href="<?php echo home_url(); ?>">資料請求</a>
+        <a class="mv_contact" href="<?php echo home_url(); ?>">お問い合わせ</a>
+    </div>
+</div>
+<div class="document_request__bottom">
+    <p class="contact_txt">お電話でのお問い合わせはこちら</p>
+    <p class="tel">012-456-789</p>
+    <p class="time">平日 08:00~20:00</p>
+</div>
